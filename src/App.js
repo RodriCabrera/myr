50aeee3c5d53cc4048bc0f88f27925608bb96a25
Rodrigo <rodrigo1cabrera2@gmail.com>
2022-02-17 17:44:21 -0300

    Remove Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import BgContainer from "./components/BgContainer";
 import Countdown from "./components/Countdown";
 import Header from "./components/Header";
-import Navbar from "./components/Navbar";
 import Regalos from "./components/Regalos";
 import RSVP from "./components/RSVP";
 import Ubicacion from "./components/Ubicacion";
@@ -10,7 +9,6 @@ function App() {
   return (
     <>
       <BgContainer />
-      <Navbar />
       <Header />
       <Countdown />
       <RSVP />
