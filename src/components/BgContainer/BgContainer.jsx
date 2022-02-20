ab6286e8b9dcb4a803adf1e3098e708ec3494c98
Rodrigo <rodrigo1cabrera2@gmail.com>
2022-02-20 19:06:36 -0300

    Modify bg container to collage

--- src/components/BgContainer/BgContainer.jsx
+++ src/components/BgContainer/BgContainer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FixedBg } from "./BgContainer.styles";
-import background from "../../assets/eliylucho.jpeg";
+import background from "../../assets/collage.png";
 
 const BgContainer = ({ children }) => {
   return <FixedBg style={{ backgroundImage: `url(${background})` }} />;
--- src/components/BgContainer/BgContainer.styles.js
+++ src/components/BgContainer/BgContainer.styles.js
@@ -4,7 +4,7 @@ export const FixedBg = styled.div`
   position: fixed;
   height: 100vh;
   width: 100vw;
-  background-size: cover;
+  background-size: contain;
   background-position: center;
   z-index: -1;
 `;
